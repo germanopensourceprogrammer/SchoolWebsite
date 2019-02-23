7f17cc4c0d35b753e9ff97f72a19718095fb901f
Jens Reetmeyer <jens@reetmeyer.de>
2019-02-23 22:21:27 +0100

    -add everything form last project

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$homepath = "Home.html";
+$contentpath = "content/Content-";
+$contentmark = "|||CONTENT|||";
+$mobilehomepath = "Mobile.html";
+//---Config End---
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+function readContent($path, $contentmark, $contentpath) {
+  $homefile = file($path);
+  foreach ($homefile as $line) {
+    if (strpos($line, $contentmark)) {
+      $speccontentpath = $contentpath . substr(substr($line, strrpos($line, $contentmark) + strlen($contentmark) + 1),0 , -3);
+      if (file_exists($speccontentpath . ".html")) {
+        echo file_get_contents($speccontentpath . ".html");
+      }else {
+        echo "<h1>Content not found</h1><br> Please Contact an Admin";
+      }
+
+    }else {
+        echo $line;
+    }
+  }
+}
+
+if (isMobile()) {
+  readContent($mobilehomepath, $contentmark, $contentpath);
+}else {
+  readContent($homepath, $contentmark, $contentpath);
+}
+
+
+ ?>
---
+++ test.js
@@ -0,0 +1,106 @@
+// Tabline with Buttons
+function openTabcontent(tagName,elmnt,color,sidenav) {
+  var i, tabcontent, tablinks;
+  var sidenavs = document.getElementsByClassName("sidenav");
+  for (i = 0; i < sidenavs.length; i++) {
+    if (sidenav) {
+      sidenavs[i].style.display = "block";
+    }else {
+      sidenavs[i].style.display = "none";
+    }
+  }
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(tagName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+  openContent("main-"+tagName);
+}
+
+function afterpageload() {
+  document.getElementById("defaultOpen").click();
+  closeSubsidelinks(1);
+  var contents = document.getElementsByClassName("content");
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
+  }
+}
+// end or Tabline
+
+// content load
+function loadContent(tagName) {
+  var i, content;
+  content = document.getElementsByClassName("content");
+  for (i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
+  }
+  document.getElementById(tagName).style.display = "block";
+}
+// end of content load
+
+// mobile dropdown menu
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function onMobileload(){
+  closeSubmenus(1);
+  var contents = document.getElementsByClassName("content");
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
+  }
+  openContent("main");
+}
+function openMobileContent(tagName, hasSubmenu, submenulevel){
+  if (hasSubmenu) {
+    if (submenulevel == null) {
+      closeSubmenus(1);
+      return;
+    }
+    for (var i = 0; i < 5; i++) {
+      closeSubmenus(submenulevel + i)
+    }
+    var submenu = document.getElementById(tagName + "-submenu");
+    submenu.style.display = "block";
+  }else {
+    var x = document.getElementById("myLinks");
+    x.style.display = "none";
+  }
+  openContent(tagName);
+}
+function closeSubmenus(submenulevel){
+  var submenus = document.getElementsByClassName("submenu-" + submenulevel);
+  for (var i = 0; i < submenus.length; i++) {
+    submenus[i].style.display = "none";
+  }
+}
+// end of mobile dropdown menu
+
+// desktop subsidemenu
+function openContent(tagName, hasSubmenu, submenulevel){
+  if (hasSubmenu) {
+    for (var i = 0; i < 5; i++) {
+      closeSubsidelinks(submenulevel + i)
+    }
+    var submenu = document.getElementById(tagName + "-subsidelink");
+    submenu.style.display = "block";
+  }else {
+    closeSubsidelinks(submenulevel);
+  }
+  loadContent(tagName);
+}
+function closeSubsidelinks(submenulevel){
+  var submenus = document.getElementsByClassName("subsidelink-" + submenulevel);
+  for (var i = 0; i < submenus.length; i++) {
+    submenus[i].style.display = "none";
+  }
+}
