dbada5ccd268cf6fc1b85cf8045270dedec20d7e
Eric <47606229+superspieler4@users.noreply.github.com>
2019-03-23 21:09:53 +0100

    -edit css with js

--- screen.js
+++ screen.js
@@ -31,13 +31,17 @@ function onload() {
     var elements = document.getElementsByName('headnav-size-middle')
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.width = reswidth - (reswidth / 100 * (13 * 2))  + "px";
-      elements[i].style.fontSize = (reswidth / 100 * 13) / 100 * 38  + "px";
+      elements[i].style.height = reswidth / 100 * 11.2  + "px";
+      elements[i].style.paddingTop = reswidth / 100 * 1.8  + "px";
+      elements[i].style.fontSize = (reswidth / 100 * 13) / 100 * 34  + "px";
     }
   } else {
     var elements = document.getElementsByName('headnav-size-middle')
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.width = reswidth - (resheight / 100 * (13 * 2))  + "px";
-      elements[i].style.fontSize = (resheight / 100 * 13) / 100 * 38  + "px";
+      elements[i].style.height = resheight / 100 * 11.2  + "px";
+      elements[i].style.paddingTop = resheight / 100 * 1.8  + "px";
+      elements[i].style.fontSize = (resheight / 100 * 13) / 100 * 34  + "px";
     }
   }
 
@@ -80,4 +84,43 @@ function onload() {
       elements[i].style.height = (resheight / 100 * 13) / 100 * 13.214  + "px";
     }
   }
+
+  if (reswidth < resheight) {
+    var elements = document.getElementsByName('menu-button-js')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.height = reswidth / 100 * 8.273  + "px";
+      elements[i].style.paddingTop = reswidth / 100 * 2.19  + "px";
+      elements[i].style.paddingBottom = reswidth / 100 * 2.19  + "px";
+      elements[i].style.paddingLeft = reswidth / 100 * 3.89  + "px";
+      elements[i].style.paddingRight = reswidth / 100 * 3.89  + "px";
+      elements[i].style.fontSize = reswidth / 100 * 3.89  + "px";
+    }
+  } else {
+    var elements = document.getElementsByName('menu-button-js')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.height = resheight / 100 * 8.273  + "px";
+      elements[i].style.paddingTop = resheight / 100 * 2.19  + "px";
+      elements[i].style.paddingBottom = resheight / 100 * 2.19  + "px";
+      elements[i].style.paddingLeft = resheight / 100 * 3.89  + "px";
+      elements[i].style.paddingRight = resheight / 100 * 3.89  + "px";
+      elements[i].style.fontSize = resheight / 100 * 3.89  + "px";
+    }
+  }
+
+  if (reswidth < resheight) {
+    var elements = document.getElementsByName('content-headline-js')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.height = reswidth / 100 * 4  + "px";
+      elements[i].style.padding = reswidth / 100 * 1.217  + "px";
+      elements[i].style.fontSize = reswidth / 100 * 4.38  + "px";
+    }
+  } else {
+    var elements = document.getElementsByName('content-headline-js')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.height = resheight / 100 * 4  + "px";
+      elements[i].style.padding = resheight / 100 * 1.217  + "px";
+      elements[i].style.fontSize = resheight / 100 * 4.38  + "px";
+    }
+  }
+
 }
