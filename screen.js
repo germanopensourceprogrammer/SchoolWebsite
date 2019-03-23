9163593b7924c9dd719b76fa1b2b51bd39242fba
Jens Reetmeyer <jens@reetmeyer.de>
2019-03-23 10:07:55 +0100

    -edit tagname support to name support -edit html tagnames to names

--- screen.js
+++ screen.js
@@ -1,6 +1,6 @@
 function onload() {
   if (screen.width < screen.height) {
-    var elements = document.getElementsByTagName('headnav-size');
+    var elements = document.getElementsByName('headnav-size')
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.width = screen.width / 100 * 13  + "px";
       elements[i].style.height = screen.width / 100 * 13  + "px";
