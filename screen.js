e01d68ba3d61ba2f5bc8a1d96195f229ff21c99d
Jens Reetmeyer <jens@reetmeyer.de>
2019-03-23 10:03:43 +0100

    -add tagname support

--- screen.js
+++ screen.js
@@ -1,7 +1,10 @@
 function onload() {
   if (screen.width < screen.height) {
-    document.getElementById('headnav-size').style.width = screen.width / 100 * 13  + "px";
-    document.getElementById('headnav-size').style.height = screen.width / 100 * 13  + "px";
+    var elements = document.getElementsByTagName('headnav-size');
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.width = screen.width / 100 * 13  + "px";
+      elements[i].style.height = screen.width / 100 * 13  + "px";
+    }
   } else {
 
   }
