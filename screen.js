86419c3c7b96ff3eba15c9973d71e97208bcc23a
Eric <47606229+superspieler4@users.noreply.github.com>
2019-03-23 09:59:49 +0100

    -add screen.js

---
+++ screen.js
@@ -0,0 +1,8 @@
+function onload() {
+  if (screen.width < screen.height) {
+    document.getElementById('headnav-size').style.width = screen.width / 100 * 13  + "px";
+    document.getElementById('headnav-size').style.height = screen.width / 100 * 13  + "px";
+  } else {
+
+  }
+}
