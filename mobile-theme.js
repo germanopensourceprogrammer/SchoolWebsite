b4016e1ee0355d7750bf94b79e95134ec8bc522a
Eric <47606229+superspieler4@users.noreply.github.com>
2019-05-01 16:02:03 +0200

    -add mobile theme option

---
+++ mobile-theme.js
@@ -0,0 +1,18 @@
+function Theme() {
+
+	if (theme = "black") {
+    var elements = document.getElementsByName('Theme_mobile-container')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "white";
+      elements[i].style.height = reswidth / 100 * 13  + "px";
+    }
+  }
+	if (theme = "light") {
+    var elements = document.getElementsByName('Theme_mobile-container')
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "white";
+			elements[i].style.height = resheight / 100 * 13  + "px";
+    }
+	
+  }
+//document.getElementsByName('Theme_mobile-container').style.color = "black";
